cfe90db6c75e717245a0c34d2c97f30d39fc78f6
ednachiang <echiang3@wisc.edu>
2020-01-22 14:09:45 -0600

    Commented code and removed all baseline values except last one right before gavage

--- scripts/import_crds.R
+++ scripts/import_crds.R
@@ -13,20 +13,42 @@ import.crds <- function(x,y) {
   for (i in 1:n){
     filename <- list.files(x)[i]
     id <- substr(filename, 1,3)
+      # Pull out squirrel ID
     wd <- paste0(x,"/",filename)
+      # Path to each csv file
     df <- assign(paste0("crds.", id), read.csv(wd, header=T, fileEncoding="UTF-8-BOM"))
+      # Save each csv file as a dataframe
     rw <- which(meta[,2] == id)
+      # Find the metadata row which matches the squirrel ID of the csv file
     
+    
+    # Remove all baseline values except for the last one right before gavage
+    bl <- which(df[,1] < 0)
+      # Identify baseline values
+    bln <- length(bl)
+      # Find number of baseline values
+    for (k in 1:(bln-1)){
+      df <- df[-1,]
+    }
+    
+    
+    # Add squirrel number ID
     for (j in 1:6){
       cl = j
       df[,j+3] <- rep(meta[rw,cl],nrow(df))
+        # Add a column of squirrel 4-digit numeric ID
       colnames(df)[j+3] <- colnames(meta)[cl]
+        # Rename the new column
     }
     
+    
+    # Save all the data in one giant dataframe
     if (i < 2){
       crds <- df
+      # Creates initial dataframe with the first file
     } else {
       crds <- rbind(crds, df)
+      # Appends onto the dataframe for all subsequent files
     }
     
   }
--- scripts/import_crds_sep.R
+++ scripts/import_crds_sep.R
@@ -13,17 +13,36 @@ import.crds.sep <- function(x,y) {
   for (i in 1:n){
     filename <- list.files(x)[i]
     id <- substr(filename, 1,3)
+      # Pull out squirrel ID
     wd <- paste0(x,"/",filename)
+      # Path to each csv file
     df <- assign(paste0("crds.", id), read.csv(wd, header=T, fileEncoding="UTF-8-BOM"))
+      # Save each csv file as a dataframe
     rw <- which(meta[,2] == id)
+      # Find the metadata row which matches the squirrel ID of the csv file
     
+    
+    # Remove all baseline values except for the last one right before gavage
+    bl <- which(df[,1] < 0)
+    # Identify baseline values
+    bln <- length(bl)
+    # Find number of baseline values
+    for (k in 1:(bln-1)){
+      df <- df[-1,]
+    }
+    
+    
+    # Add squirrel number ID
     for (j in 1:6){
       cl = j
       df[,j+3] <- rep(meta[rw,cl],nrow(df))
+        # Add a column of squirrel 4-digit numeric ID
       colnames(df)[j+3] <- colnames(meta)[cl]
+        # Rename the new column
     }
     
     crds[[i]] <- assign(paste0("crds.", id), df)
+      # Save each crds in a list
     
   }
   
