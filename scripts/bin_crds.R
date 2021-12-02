2ba79cf457c7dedee3f783333238f81e42b2958b
ednachiang <echiang3@wisc.edu>
2021-12-02 14:58:25 -0600

    Removed mannitol

--- scripts/bin_crds.R
+++ scripts/bin_crds.R
@@ -6,27 +6,22 @@ bin.crds <- function(x){
   
   # Separate Inulin, Mannitol, and Saline
   inu <- crds[which(crds$Substrate == "Inulin"),]
-  man <- crds[which(crds$Substrate == "Mannitol"),]
   sal <- crds[which(crds$Substrate == "Saline"),]
   
   # Make new col for BinTime
   inu$BinTime <- 0
-  man$BinTime <- 0
   sal$BinTime <- 0
   
   # Pull out baselines
   inu.base <- inu[which(inu$RelTime < 0),]
-  man.base <- man[which(man$RelTime < 0),]
   sal.base <- sal[which(sal$RelTime < 0),]
   
   # Calculate mean RelTime and add as new BinTime in each substrate's dataframe
   inu$BinTime[which(inu$RelTime < 0)] <- mean(inu.base$RelTime)
-  man$BinTime[which(man$RelTime < 0)] <- mean(man.base$RelTime)
   sal$BinTime[which(sal$RelTime < 0)] <- mean(sal.base$RelTime)
   
   # Pull out post-gavage timepoints (a.k.a. experimental timepoints)
   inu.exp <- inu[which(inu$RelTime > 0),]
-  man.exp <- man[which(man$RelTime > 0),]
   sal.exp <- sal[which(sal$RelTime > 0),]
   
   
@@ -40,16 +35,6 @@ bin.crds <- function(x){
   inu.spr <- inu.exp[which(inu.exp$Season == "Spring"),]
   inu.spr.noabx <- inu.spr[which(inu.spr$Antibiotics == "No"),]
   inu.spr.abx <- inu.spr[which(inu.spr$Antibiotics == "Yes"),]
-  
-  man.sum <- man.exp[which(man.exp$Season == "Summer"),]
-  man.sum.noabx <- man.sum[which(man.sum$Antibiotics == "No"),]
-  man.sum.abx <- man.sum[which(man.sum$Antibiotics == "Yes"),]
-  man.win <- man.exp[which(man.exp$Season == "Winter"),]
-  man.win.noabx <- man.win[which(man.win$Antibiotics == "No"),]
-  man.win.abx <- man.win[which(man.win$Antibiotics == "Yes"),]
-  man.spr <- man.exp[which(man.exp$Season == "Spring"),]
-  man.spr.noabx <- man.spr[which(man.spr$Antibiotics == "No"),]
-  man.spr.abx <- man.spr[which(man.spr$Antibiotics == "Yes"),]
   
   sal.sum <- sal.exp[which(sal.exp$Season == "Summer"),]
   sal.sum.noabx <- sal.sum[which(sal.sum$Antibiotics == "No"),]
@@ -65,7 +50,6 @@ bin.crds <- function(x){
   # Bin data into equally spaced bins; Take mean of each bin to put into BinTime
   # Number of bins is based upon what I did previously (every 30 min) & ensures at least 3 data points per bin
   # Inulin = 9
-  # Mannitol = 4
   # Saline = 6
   # ( = open
   # [ = closed
@@ -76,13 +60,6 @@ bin.crds <- function(x){
   inu.win.abx$BinTime <- ave(inu.win.abx$RelTime, cut(inu.win.abx$RelTime, breaks=9), FUN=mean)
   inu.spr.noabx$BinTime <- ave(inu.spr.noabx$RelTime, cut(inu.spr.noabx$RelTime, breaks=9), FUN=mean)
   inu.spr.abx$BinTime <- ave(inu.spr.abx$RelTime, cut(inu.spr.abx$RelTime, breaks=9), FUN=mean)
-  
-  man.sum.noabx$BinTime <- ave(man.sum.noabx$RelTime, cut(man.sum.noabx$RelTime, breaks=4), FUN=mean)
-  man.sum.abx$BinTime <- ave(man.sum.abx$RelTime, cut(man.sum.abx$RelTime, breaks=4), FUN=mean)
-  man.win.noabx$BinTime <- ave(man.win.noabx$RelTime, cut(man.win.noabx$RelTime, breaks=4), FUN=mean)
-  man.win.abx$BinTime <- ave(man.win.abx$RelTime, cut(man.win.abx$RelTime, breaks=4), FUN=mean)
-  man.spr.noabx$BinTime <- ave(man.spr.noabx$RelTime, cut(man.spr.noabx$RelTime, breaks=4), FUN=mean)
-  man.spr.abx$BinTime <- ave(man.spr.abx$RelTime, cut(man.spr.abx$RelTime, breaks=4), FUN=mean)
   
   sal.sum.noabx$BinTime <- ave(sal.sum.noabx$RelTime, cut(sal.sum.noabx$RelTime, breaks=6), FUN=mean)
   sal.sum.abx$BinTime <- ave(sal.sum.abx$RelTime, cut(sal.sum.abx$RelTime, breaks=6), FUN=mean)
@@ -121,25 +98,7 @@ bin.crds <- function(x){
     inu$BinTime[which(rownames(inu) == rownames(match_df(inu[,1:14], inu.spr.abx[i,1:14])))] <- inu.spr.abx$BinTime[i]
   }
   
-  ## Mannitol
-  for(i in 1:nrow(man.sum.noabx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.sum.noabx[i,1:14])))] <- man.sum.noabx$BinTime[i]
-  }
-  for(i in 1:nrow(man.sum.abx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.sum.abx[i,1:14])))] <- man.sum.abx$BinTime[i]
-  }
-  for(i in 1:nrow(man.win.noabx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.win.noabx[i,1:14])))] <- man.win.noabx$BinTime[i]
-  }
-  for(i in 1:nrow(man.win.abx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.win.abx[i,1:14])))] <- man.win.abx$BinTime[i]
-  }
-  for(i in 1:nrow(man.spr.noabx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.spr.noabx[i,1:14])))] <- man.spr.noabx$BinTime[i]
-  }
-  for(i in 1:nrow(man.spr.abx)) {
-    man$BinTime[which(rownames(man) == rownames(match_df(man[,1:14], man.spr.abx[i,1:14])))] <- man.spr.abx$BinTime[i]
-  }
+  print(inu)
   
   ## Saline
   for(i in 1:nrow(sal.sum.noabx)) {
@@ -162,7 +121,7 @@ bin.crds <- function(x){
   }
 
   
-  output <- rbind(inu,man,sal)
+  output <- rbind(inu,sal)
   return(output)
   
   
