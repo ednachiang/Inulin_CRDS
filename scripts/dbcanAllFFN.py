4e9c744f2feee9625d1a2f3dbfe96ec535674832
ednachiang <echiang3@wisc.edu>
2022-02-04 19:11:05 -0600

    Added files from OG_MetaG

---
+++ scripts/dbcanAllFFN.py
@@ -0,0 +1,105 @@
+import os
+	# Import os to iterate over files in a directory
+import pandas as pd
+    # Import pandas to use dataframes
+
+##### CHANGE THESE PARAMETERS ACCORDINGLY
+directorydbCAN = '/home/glbrc.org/echiang3/Inulin_MetaG/dbcan/parsed/'
+    # Directory of dbCAN.parsed.txt
+directoryProdigal = '/home/glbrc.org/echiang3/Inulin_MetaG/prodigal/ffn/'
+    # Directory of prodigal .ffn output files
+directoryOutput = '/home/glbrc.org/echiang3/Inulin_MetaG/dbcan/ffn/'
+    # Path to output dbCAN .ffn files
+
+# Input = list of CAZymes that appear in only 1 sample. These are removed in my R analysis.
+drop = ["GH49", "GT52", "GT7", "GT73", "GT89", "PL31", "CE5", "GH232", "GH157", "GT44", "GT61"]
+
+
+
+##### Define function: Pull out dbCAN-classified ORFs to create ffn of dbCAN genes
+def parse_dbCAN_ORFs(dbCAN_parsed_txt_path, prodigal_path, output_path):
+
+    ##### Identify unwanted ORFs #####
+    # Open input dbCAN file
+    dbCANinput = open(dbCAN_parsed_txt_path, mode = 'r')
+
+    # Create empty list for ORFs to remove
+    remove = []
+
+    for line1 in dbCANinput.readlines():
+        # Read file line-by-line
+        col = line1.split('\t')
+            # Split row by tabs
+        if 'hmm' in col[0]:
+            family = col[0].split('.', 1)[0]
+                # Pull out everything before the ".hmm"
+            for CAZyme in drop:
+                if family == CAZyme:
+                    removeTrim = col[2]
+                    removeTrim = removeTrim[:-1]
+                        # Remove new line character at end of ORF name
+                    remove.append(removeTrim)
+
+
+    ##### Pull out ORF names #####
+    # Open input dbCAN file
+    dbCANinput = open(dbCAN_parsed_txt_path, mode = 'r')
+
+    # Create empty list for ORFs
+    ORFs = []
+
+    # Pull out ORFs classified to CAZyme
+    for line1 in dbCANinput.readlines():
+        # Read file line-by-line
+        col = line1.split('\t')
+            # Split row by tabs
+        if 'hmm' in col[0]:
+            ORFtrim = col[2]
+            ORFtrim = ORFtrim[:-1]
+                # Remove new line character at end of ORF name
+            ORFs.append(ORFtrim)
+    
+
+    ##### Remove unwanted ORFs #####
+    for ORF0 in ORFs:
+        for ORF_remove in remove:
+            if ORF0 == ORF_remove:
+                ORFs.remove(ORF0)
+
+
+    ##### Create ffn file #####
+    # Open output .ffn file
+    output = open(output_path, mode = 'a')
+
+    for ORF1 in ORFs:
+        # Go ORF-by-ORF through ORF list
+        prodigalInput = open(prodigal_path, mode = 'r')
+        flag = False
+        
+        for line2 in prodigalInput.readlines():
+            # Iterate line-by-line through prodigal file
+            if ORF1 in line2:
+                flag = True
+                output.write(line2)
+                continue
+            if '>' in line2 and flag == True:
+                prodigalInput.close()
+                break
+            if flag == True:
+                output.write(line2)
+    return(output)
+
+
+##### Run Code #####
+# Create .ffn of mucin-degrading enzyme genes
+for file in os.listdir(directorydbCAN):
+    # Iterate for all files in directory
+    sample = str(file[:5])
+    dbCANpath = directorydbCAN + str(file[:5]+'.dm.parsed')
+    print(dbCANpath)
+    prodigalPath = directoryProdigal + sample + '.ffn'
+    print(prodigalPath)
+    outputPath = directoryOutput + sample + '.ffn'
+    print(outputPath)
+    parse_dbCAN_ORFs(dbCANpath, prodigalPath, outputPath)
+        # Function will save the .ffn file, so no need to set it to a variable
---
+++ scripts/dbcanSignificantFFN.py
@@ -0,0 +1,101 @@
+import os
+	# Import os to iterate over files in a directory
+import pandas as pd
+    # Import pandas to use dataframes
+
+##### CHANGE THESE PARAMETERS ACCORDINGLY
+directorydbCAN = '/mnt/bigdata/linuxhome/echiang3/Inulin_MetaG/dbcan/parsed/'
+    # Directory of dbCAN.parsed.txt
+dbCANtax = '/mnt/bigdata/linuxhome/echiang3/Inulin_MetaG/dbcan/output/dbcan_tax_table.csv'
+    # dbCAN "tax" table
+directoryFFN = '/mnt/bigdata/linuxhome/echiang3/Inulin_MetaG/dbcan/ffn/'
+    # Directory of dbcanGetFFN.py .ffn output files
+directoryOutput = '/mnt/bigdata/linuxhome/echiang3/Inulin_MetaG/dbcan/sig/'
+    # Path to output dbCAN .ffn files
+
+# Input = list of significant CAZyme families
+CAZymes = ["CE12", "CE2", "GH3", "GH5", "GH8", "GH9", "GH10", "GH11", "GH20", "GH25", "GH26", "GH31", "GH33", "GH42", "GH43", "GH44", "GH47", "GH51", "GH84", "GH85", "GH88", "GH89", "GH92", "GH94", "GH101", "GH109", "GH110", "GH115", "GT11", "GT24", "GT94", "PL1", "PL8", "PL9", "PL11", "PL12"]
+
+##### Define function: Pull out dbCAN-classified ORFs to create ffn of dbCAN genes
+def parse_CAZyme_ORFs(dbCAN_parsed_txt_path, dbCAN_tax_table_path, dbCAN_ffn_path, output_path):
+
+    # Save paths for all dbCAN.parsed.txt files
+    dbCANfiles = []
+    for file1 in os.listdir(dbCAN_parsed_txt_path):
+        dbCANpath = dbCAN_parsed_txt_path + file1
+        dbCANfiles.append(dbCANpath)
+    
+    # Create number list for 'for' loops to iterate through all dbCAN.parsed.txt and dbcanffn/ffn files files
+    fileNumber = range(0,len(dbCANfiles))
+
+    for file2 in fileNumber:
+        # Save sample ID
+        sample = str(dbCANfiles[file2][-15:-11])
+
+        # Create empty dictionary for parsed dbCAN ORFs
+        dbCAN_ORFs = { }
+
+        # Open input file
+        dbCANinput = open(dbCANfiles[file2])
+        print(dbCANfiles[file2])
+
+        # Pull out ORFs classified to sig CAZymes
+        for line1 in dbCANinput.readlines():
+            # Read file line-by-line
+
+            col = line1.split('\t')
+                # Split the row by tabs
+            
+            if 'hmm' in col[1]:
+                # Pull out only lines that specify dbCAN classification in col 2.
+                family = col[1].split('.', 1)[0]
+                    # Pull out everything before the ".hmm"
+                
+                for entry1 in CAZymes:
+                    # Go entry-by-entry through CAZyme list
+                    if entry1 == family:
+                        dbCAN_ORF = line1.split()
+                        dbCAN_ORFs[dbCAN_ORF[4]] = entry1
+                            # Add ORF to dictionary of dbCAN ORFs
+        
+        # Convert dictionary to dataframe
+        df1 = pd.DataFrame.from_dict(dbCAN_ORFs, orient = 'index', columns = ['CAZyme'])
+
+        for entry2 in CAZymes:
+            # Go entry-by-entry through list of significant CAZymes
+            outputPath = output_path + sample + '.' + entry2 + '.ffn'
+                # Name output file
+            outputFFN = open(outputPath, mode = 'a')
+                # Open output file
+            
+            # Create list of ORFs classified to entry2
+            sigORF = []
+
+            for row in df1.index:
+                # GO line-by-line through df1
+                if entry2 == df1.at[row, 'CAZyme']:
+                    sigORF.append(row)
+                        # Create list of ORFs classified to a specific CAZyme
+                
+            for ORF1 in sigORF:
+                inputPath = dbCAN_ffn_path + sample + '.ffn'
+                ffn_input = open(inputPath, mode = 'r')
+                flag = False
+
+                for line2 in ffn_input.readlines():
+                    # Iterate line-by-line through .ffn file
+                    if ORF1 in line2:
+                        flag = True
+                        outputFFN.write(line2)
+                        continue
+                    if '>' in line2 and flag == True:
+                        ffn_input.close()
+                        break
+                    if flag == True:
+                        outputFFN.write(line2)
+  
+
+    
+##### Run code #####
+parse_CAZyme_ORFs(directorydbCAN, dbCANtax, directoryFFN, directoryOutput)
+        # Function will save .ffn files, so no need to set it to a variable
