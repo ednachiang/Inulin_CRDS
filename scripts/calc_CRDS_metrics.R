d3644573c103383a2c2901b2a38f3dd54713a89a
ednachiang <echiang3@wisc.edu>
2022-04-17 13:18:09 -0500

    Combined delta metrics + AUC calcs into 1 script

---
+++ scripts/calc_CRDS_metrics.R
@@ -0,0 +1,66 @@
+### x = dataframe output of norm.time.crds()
+
+calc.CRDS.metrics <- function(x){
+  
+  # Pull out squirrel IDs
+  IDs <- levels(as.factor(x$ID))
+  
+  # Create output dataframe
+  output <- data.frame(matrix(ncol = 5, nrow = length(levels(IDs))))
+  colnames(output) <- c("ID", "AvgDelta", "MaxD",  "MaxSlope", "AUC")
+  
+  
+  # Iterate through each squirrel ID
+  for(i in 1:length(IDs)){
+    curve <- which(x$ID == IDs[i])
+    curve.df <- x[curve,]
+      # Pull out measurements for the individual squirrel
+    curve.df <- curve.df[-2,]
+      # Remove first measurement after gavage because it's always weird
+    
+    # Pull out average max delta
+    maxD_sorted <- sort(curve.df$DeltaNorm[2:nrow(curve.df)], decreasing = T)
+    maxDuse <- mean(maxD_sorted[1:3])
+    
+    # Calculate average delta
+    meanD <- mean(curve.df$DeltaNorm[2:nrow(curve.df)])
+
+    
+    # Calculate average maximum slope
+    slope = 0
+    for(j in 3:(nrow(curve.df))){
+        # Go row-by-row through curve.df
+      rise <- curve.df$DeltaNorm[j] - curve.df$DeltaNorm[j-1]
+      run <- curve.df$RelTime[j] - curve.df$RelTime[j-1]
+      slope <- c(slope, (rise/run) )
+      }
+    
+    slope <- slope[-1]
+      # Remove initial 0
+    slopeSort <- sort(slope, decreasing = T)
+    maxSlope <- mean(slopeSort[1:3])
+
+    
+    
+    ### Calculate AUC ###
+    # Create auc variable
+    AUC <- 0
+    # Run trapezoid method integration - more accurate
+    for(k in 2:(nrow(curve.df)-1)){
+      # Ignore baseline to first measurement
+      avgH <- (curve.df$DeltaNorm[k+1]+curve.df$DeltaNorm[k])/2
+      # Calculates average height between start and end point
+      width <- (curve.df$RelTime[k+1]-curve.df$RelTime[k])
+      AUC <- AUC + (avgH*width)
+    }
+
+
+
+    # Populate output dataframe
+    output[i,] <- c(IDs[i], meanD, maxDuse, maxSlope, AUC)
+    
+  }
+  
+  return(output)
+  
+}
