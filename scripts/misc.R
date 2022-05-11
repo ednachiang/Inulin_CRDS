388683daa35012ff20925682db435021423b4533
ednachiang <echiang3@wisc.edu>
2022-05-11 17:15:41 -0500

    Added 95% CI code

--- scripts/misc.R
+++ scripts/misc.R
@@ -9,3 +9,15 @@ g_legend <- function(a.gplot){
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)}
+
+# Calculate 95% confidence intervals
+# Code from = https://ggplot2tutor.com/tutorials/summary_statistics
+CI.upper <- function(x){
+  mean(x) + 
+    qt(.975, df = length(x)) * sd(x) / sqrt(length(x))
+} 
+
+CI.lower <- function(x){
+  mean(x) - 
+    qt(.975, df = length(x)) * sd(x) / sqrt(length(x))
+} 
